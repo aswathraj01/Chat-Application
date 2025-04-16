d72a71829785098dda01e0cc05a0bb400a56062c
Aswath Raj <aswathraj749@gmail.com>
2025-04-16 23:03:59 +0530

    admin panel in work
    setting the main admin dash

--- chatbackend/admin_web/views.py
+++ chatbackend/admin_web/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.db.models import Sum
+from support.models import SupportRequest
+ 
+
 
 def admin_login(request):
     if request.method == 'POST':
@@ -18,14 +22,19 @@ def admin_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'admin_web/dashboard.html')
+    CustomUser = get_user_model()
+    total_users = CustomUser.objects.count()
+    total_logins = CustomUser.objects.aggregate(Sum('login_count'))['login_count__sum'] or 0
+    total_requests = SupportRequest.objects.count()
+
+    return render(request, 'admin_web/dashboard.html', {
+        'total_users': total_users,
+        'total_logins': total_logins,
+        'total_requests': total_requests,
+    })
 
 @login_required
 def admin_logout(request):
     logout(request)
     return redirect('admin_login')
 
-def admin_dashboard(request):
-    CustomUser = get_user_model()
-    total_users = CustomUser.objects.count()
-    return render(request, 'admin/dashboard.html', {'total_users': total_users})
